33facb2323ed9b5b623a1a6241f88e0e074a1309
Le Thi Thanh Xuan (Hellen)/SGVN QA Team <lexuan.vn@smilegate.com>
2025-11-14 18:00:24 +0700

    14 Nov

--- tests1/features/test_feature.feature
+++ tests1/features/test_feature.feature
@@ -2,4 +2,4 @@ Feature: This test to cover login feature example
   Scenario: Run login test case example
     Given I go to partner login page
     When I input valid credential on the partner login page
-    Then I click login button
+
--- tests1/steps/test_login_steps.py
+++ tests1/steps/test_login_steps.py
@@ -3,16 +3,17 @@ import pytest
 from playwright.sync_api import Page
 from pages.login_page import LoginPage
 
-scenarios()
-@pytest.fixture(scope="module")
+scenarios("../features/test_feature.feature")
+@pytest.fixture()
 def login_page(page:Page):
     return LoginPage(page)
+
 @given("I go to partner login page")
 def i_go_to_partner_login_page(login_page):
    login_page.goto_baseurl()
 
 @when("I input valid credential on the partner login page")
 def i_input_valid_credential(login_page):
-    login_page.login()
+    login_page.login("lexuan.vn@smilegate.com","Hoilamgi123!")
 
 
